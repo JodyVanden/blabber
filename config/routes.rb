aafe465b8ce0f782f827bf9957078fb8ccd83674
Jody <jody@fresho.com>
2025-03-16 19:10:20 +1100

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :posts, only: %i[index create] do
+    get 'like'
+    get 'repost'
+  end
+
+  root to: 'posts#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
